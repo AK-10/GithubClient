653d79cd5b02ebae552eb78c881ea382cc2bfa84
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-10-19 23:29:03 +0900

    impl fav button

--- GithubClient/Views/RepositoryCollectionCell/RepositoryCollectionViewCell.swift
+++ GithubClient/Views/RepositoryCollectionCell/RepositoryCollectionViewCell.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import SwiftIcons
 
 class RepositoryCollectionViewCell: UICollectionViewCell {
     
@@ -15,21 +17,30 @@ class RepositoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var languageLabel: UILabel!
     @IBOutlet private weak var starNumLabel: UILabel!
     @IBOutlet private weak var languageColorView: UIView!
+    @IBOutlet weak var favButton: UIButton!
     
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        repositoryLabel.text = ""
-//        descriptionLabel.text = ""
-//        languageLabel.text = ""
-//        starNumLabel.text = ""
-//        languageColorView.backgroundColor = .white
-//    }
+    private let disposeBag = DisposeBag()
     
     func setup(repository: Repository) {
         repositoryLabel.text = repository.repositoryName
         descriptionLabel.text = repository.description
         languageLabel.text = repository.language
         starNumLabel.text = repository.starNum.description
+        favorite()
+    }
+    
+    func favorite() {
+        favButton.tintColor = .systemPink
+        let fillFavIcon = UIImage(bgIcon: .fontAwesomeSolid(.heart), bgTextColor: .systemPink, bgBackgroundColor: .clear, topIcon: .fontAwesomeSolid(.heart), topTextColor: .clear)
+
+        favButton.setBackgroundImage(fillFavIcon, for: .normal)
+    }
+    
+    func unfavorite() {
+        favButton.tintColor = .lightGray
+        let favIcon = UIImage(bgIcon: .fontAwesomeRegular(.heart), bgTextColor: .lightGray, bgBackgroundColor: .clear, topIcon: .fontAwesomeRegular(.heart), topTextColor: .clear)
+        favButton.setBackgroundImage(favIcon, for: .normal)
+        favButton.tintColor = .lightGray
     }
     
 }
